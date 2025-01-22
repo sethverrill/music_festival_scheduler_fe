52e75d4d481ced22d73f3fc8d8fd77fa17f77972
sethverrill <verrill.seth@gmail.com>
2025-01-21 18:40:01 -0700

    Add fetch request to users index and add error messages to apply to UserIndex

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from 'react-router-dom';
 import UserIndex from './components/UserIndex';
 import UserItinerary from './components/UserItinerary';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/v1/users")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data.data);
+      })
+      .catch((error) => setError(error.message));
+  }, []);
+
   return (
     <body>
       <header>
@@ -13,7 +30,7 @@ function App() {
       <section>
         <Routes>
           <Route path="/" element={<UserIndex />} />
-          <Route path="/user/:id" element={UserItinerary />} />
+          <Route path="/user/:id" element={<UserItinerary />} />
         </Routes>
       </section>
     </body>
