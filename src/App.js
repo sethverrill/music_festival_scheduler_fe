efa875a4f4660f7e73630c3785b2020737a80316
sethverrill <verrill.seth@gmail.com>
2025-01-21 18:27:40 -0700

    Start basic layout of front page with routing added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+import UserIndex from './components/UserIndex';
+import UserItinerary from './components/UserItinerary';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <header>
+        <h1>Music Festive Scheduler</h1>
+        <h2>Admin Panel</h2>
       </header>
-    </div>
-  );
+      <section>
+        <Routes>
+          <Route path="/" element={<UserIndex />} />
+          <Route path="/user/:id" element={UserItinerary />} />
+        </Routes>
+      </section>
+    </body>
+  )
 }
 
 export default App;
